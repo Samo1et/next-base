125c2fe4c81d3e0ce0767faf71d193f2c4d31d2c
d.samoylis <d.samoylis@cardsmobile.ru>
2021-12-22 20:44:52 +0700

    fix building

--- README.md
+++ README.md
@@ -10,6 +10,12 @@ npm run dev
 yarn dev
 ```
 
+## Using Docker
+
+1. [Install Docker](https://docs.docker.com/get-docker/) on your machine.
+1. Build your container: `docker build -t nextjs-docker .`.
+1. Run your container: `docker run -p 3000:3000 nextjs-docker`.
+
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
--- pages/users/[uid].jsx
+++ pages/users/[uid].jsx
@@ -53,23 +53,3 @@ export default function User(props) {
     )
 }
 
-// id: 1,
-// name: 'Leanne Graham',
-// username: 'Bret',
-// email: 'Sincere@april.biz',
-// password: '',
-// address: {
-//   street: 'Kulas Light',
-//   suite: 'Apt. 556',
-//   city: 'Gwenborough',
-//   zipcode: '92998-3874',
-//   geo: [Object]
-// },
-// phone: '1-770-736-8031 x56442',
-// website: 'hildegard.org',
-// company: {
-//   name: 'Romaguera-Crona',
-//   catchPhrase: 'Multi-layered client-server neural-net',
-//   bs: 'harness real-time e-markets'
-// }
-// }
